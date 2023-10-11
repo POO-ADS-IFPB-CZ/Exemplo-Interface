147f3b32cca2f641005e934c7986e2db35c2dabb
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-10-11 09:08:06 -0300

    Fazendo exemplo do modelo de interfaces

---
+++ src/model/Professor.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Professor extends Usuario implements Autenticavel {
+
+    private int matricula;
+    private String formacao;
+    private String email;
+    private String senha;
+
+    public Professor(String cpf, String nome, int matricula, String formacao, String email, String senha) {
+        super(cpf, nome);
+        this.matricula = matricula;
+        this.formacao = formacao;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean autenticar(String email, String senha) {
+        return this.email.equals(email) && this.senha.equals(senha);
+    }
+}
